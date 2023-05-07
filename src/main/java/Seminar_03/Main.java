a62eeb4e898dac41e2716fa579a82bf297d41863
Kalpakbaev <kozha_1992@mail.ru>
2023-05-07 11:18:53 +0500

    Hw_OOP3

--- src/main/java/Seminar_03/Main.java
+++ src/main/java/Seminar_03/Main.java
@@ -1,2 +1,36 @@
-package Seminar_03;public class Main {
+package Seminar_03;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Personal personal = new Personal();
+        personal.addUser(new User("Ivan", "Ivanov", 24)).addUser(new User("Petr", "Petrov", 25)).addUser(new User("Ira", "Semenova", 18)).addUser(new User("Artyom", "PV", 35)).addUser(new User("Artyom", "Pv", 46));
+        // for (User user:personal) {
+        //   System.out.println(user);
+        //}
+       /* for(int i = 0; i < personal.users.size();i++){
+            System.out.println(personal.users.get(i));
+        }*/
+        // List<User> users = personal.toList();
+        //  System.out.println("___________________");
+        //  System.out.println(users);
+        //  System.out.println("___________________");
+        //  Collections.sort(users);
+        //  System.out.println(users);
+        Personal personal2 = new Personal();
+        personal2.addUser(new User("Tanya", "Ivanova", 18));
+        User user = new User("Lena", "Petrova", 25);
+        user.setSubordinate(personal);
+        personal2.addUser(user);
+        User boss = new User("BOss", "Big", 32);
+        boss.setSubordinate(personal2);
+
+        Company company = new Company(boss);
+        for (User subordinates : company) {
+            System.out.println(subordinates);
+        }
+    }
+
 }
--- src/main/java/Seminar_03/User.java
+++ src/main/java/Seminar_03/User.java
@@ -1,2 +1,37 @@
-package Seminar_03;public class User {
+package Seminar_03;
+
+public class User implements Comparable<User> {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Personal getSubordinate() {
+        return subordinate;
+    }
+
+    public void setSubordinate(Personal subordinate) {
+        this.subordinate = subordinate;
+    }
+
+    private Personal subordinate;
+
+    public User(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("\nFirst name: %s, Last name: %s, age: %d", firstName, lastName, age);
+    }
+
+    @Override
+    public int compareTo(User o) {
+        int compareNames = this.firstName.compareTo(o.firstName);
+        if (compareNames != 0) return compareNames;
+        int compareLastNames = this.lastName.compareTo(o.lastName);
+        if (compareLastNames != 0) return compareLastNames;
+        return this.age - o.age;
+    }
 }
